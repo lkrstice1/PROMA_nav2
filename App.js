750c63d869c031eeed62f7c99ada59c1dc4f9aa6
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-06 23:11:37 +0200

    Kraj

--- App.js
+++ App.js
@@ -2,21 +2,51 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import PocetniEkran from './screens/PocetniEkran';
 import PopisEkran from './screens/PopisEkran';
 import DetaljiEkran from './screens/DetaljiEkran';
 import UnosEkran from './screens/UnosEkran';
+
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator()
+
+const tabOptions = ({route}) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let imeIkone;
+    if(route.name === 'Naslovna') {
+      imeIkone = focused
+        ? 'home'
+        : 'home-outline';
+    } else if (route.name === 'Detalji') {
+      imeIkone = focused ? 'list-circle' : 'list-circle-outline';
+    }
+    return <Ionicons name={imeIkone} size={size} color={color} />
+  },
+  tabBarActiveTintColor: 'green',
+  tabBarInactiveTintColor: 'red',
+  }
+);
+
+const TabEkrani = () => {
+  return(
+    <Tab.Navigator screenOptions={{
+      headerShown: false,
+    }}>
+        <Tab.Screen name="Popis" component={PopisEkran} />
+        <Tab.Screen name="Unos" component={UnosEkran} />
+    </Tab.Navigator>
+  )
+}
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Naslovna" component={PocetniEkran} />
-        <Stack.Screen name="Popis" component={PopisEkran} />
-        <Stack.Screen name="Detalji" component={DetaljiEkran} />
-        <Stack.Screen name="Unos" component={UnosEkran} />
+        <Stack.Screen name="Radovi" component={TabEkrani} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/PocetniEkran.js
+++ screens/PocetniEkran.js
@@ -1,10 +1,26 @@
-import * as React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 
 const PocetniEkran = ({navigation}) => {
+  const [brojac, postaviBrojac] = useState(0)
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={() => postaviBrojac((br) => br + 1)}>
+          <View>
+            <MaterialIcons name='add-circle' size={26} color="black" />
+          </View>
+        </TouchableOpacity>
+      )
+    });
+  }, [navigation])
+
   return (
     <View style={stil.ekran}>
       <Text>Početni ekran aplikacije</Text>
+      <Text style={stil.brojac}>Vrijednost brojača: {brojac}</Text>
       <Button title="Popis radova" onPress={() => navigation.navigate("Popis")} />
     </View>
   );
